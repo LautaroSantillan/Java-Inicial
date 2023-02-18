a08708bc56e66b6c0db80ef00ae0a2fb6feafd5b
lautarosantillan <santillanlautaro03@gmail.com>
2023-02-18 16:43:43 -0300

    Added extras challenge numbers 9 & 10 -v4

---
+++ EjerciciosExtras/Ejercicio10/src/Main.java
@@ -0,0 +1,16 @@
+/*
+Ejercicio 10 - matematicas.
+
+        Se solicita crear un programa que realice lo siguiente.
+
+        1 - Se deben cargar 2 arreglos manualmente numericos, ambos arreglos deben tener la misma longitud y sera determinada por el usuario.
+
+        2 - Se deberá mostrar la sumatoria de todos los elementos contenidos en cada arreglo.
+
+        3 - Se deberá mostrar que arreglo contiene el mayor y el menor numero de los 2
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ EjerciciosExtras/Ejercicio9/src/Main.java
@@ -0,0 +1,18 @@
+/*
+Ejercicio 9 - Arreglos
+
+        Se solicita crear un programa que verifique si 2 arreglos son inversamente iguales, para esto se debe tener en cuenta lo siguiente:
+
+        * arr1 = [0,1,2,3,4,5,6,7,8,9] y arr2 = [9,8,7,6,5,4,3,2,1,0] , estos son los 2 arreglos a modo ilustrativo.
+
+        El programa deberá iterar usando un bucle for y deberá comprobar que ambos arreglos sean inversamente iguales,
+
+        es decir el primer elemento de arr1 coincide con el ultimo de arr2, y asi sucesivamente por todos los elementos,
+
+        si se cumple la igualdad se debe mostrar que ambos son inversamente iguales, caso contrario, no son inversamente iguales.
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
