2bff739d1b3189bca4687569c9d723640454d977
lautarosantillan <santillanlautaro03@gmail.com>
2023-02-25 22:02:08 -0300

    Completed extra challenge number 9 -v6

--- EjerciciosExtras/Ejercicio9/src/Main.java
+++ EjerciciosExtras/Ejercicio9/src/Main.java
@@ -13,6 +13,21 @@ Ejercicio 9 - Arreglos
 */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Arrays & variables
+        int[] arr1 = {0,1,2,3,4,5,6,7,8,9};
+        int[] arr2 = {9,8,7,6,5,4,3,2,1,0};
+        int cont = 0;
+
+        for (int i=0; i<10; i++){
+            if (arr1[i]==arr2[10-1-i]){
+                cont++;
+            }
+        }
+
+        if (cont==10){
+            System.out.println("Ambos son inversamente iguales");
+        } else {
+            System.out.println("No son inversamente iguales");
+        }
     }
 }
